8e7eec03d4e1856ff782ab912b9d24d8415d5d72
= <=>
2014-11-23 22:19:30 +0100

    Added more headers

--- ip-headers.h
+++ ip-headers.h
@@ -31,7 +31,6 @@ typedef struct tcp_header{
 	unsigned int ack_number:32;
 	unsigned char offset:4;
 	unsigned char reserved:4;
-	// cambiar el orden en que se definen los flags //
 	unsigned char flag_fin:1;
 	unsigned char flag_syn:1;
 	unsigned char flag_rst:1;
@@ -53,6 +52,48 @@ typedef struct icmp_header{
     unsigned int specific_information:32;
 }ICMP_HEADER,*P_ICMP_HEADER;
 
+typedef struct udp_header{
+	unsigned short source_port;
+	unsigned short remote_port;
+	unsigned short datgram_length;
+	unsigned short checksum;
+}UDP_HEADER,*P_UDP_HEADER;
+
+typedef struct eth_frame{
+	unsigned long remote_mac:48;
+	unsigned long source_mac:48;
+	unsigned short type;
+	unsigned int frame_check_sequence;
+}ETH_FRAME,*P_ETH_FRAME;
+
+typedef struct dhcp_header{
+	unsigned char opcode;
+	unsigned char htype;
+	unsigned char hlen;
+	unsigned char hops;
+	unsigned int transaction_id;
+	unsigned short seconds;
+	unsigned short flags;
+	unsigned int client_addr;
+	unsigned int assigned_addr;
+	unsigned int boot_server_addr;
+	unsigned int relay_agent_addr;
+	unsigned long long int client_hw_addr;
+	unsigned char[64] server_host_name;
+	unsigned char[128] boot_filename;
+}DHCP_HEADER,*P_ETH_FRAME;
+
+/** DNS session layer (test) **/
+typedef struct dns_header{
+	unsigned short identification;
+	unsigned char qr_flags:1;
+	unsigned char opcode:4;
+	unsigned char dns_flags:7;
+	unsigned char rcode:4;
+	unsigned short total_questions;
+	unsigned short total_answers;
+	unsigned short total_authority;
+	unsigned short total_additional;
+}DNS_HEADER,*P_DNS_HEADER;
+
 // Añadir más headers //
-
-
